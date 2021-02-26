998448716dd2eed1684ecaf28b0f92ed4a0ac8bc
songyifeng <songyifeng@didiglobal.com>
2021-02-26 19:46:50 +0800

    fix  Wildcard import

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -21,11 +21,27 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -74,9 +90,11 @@ fun MyApp(puppyViewModel: PuppyViewModel = viewModel()) {
                     title = { Text(text = p.name) }
                 )
                 PuppyDetail(p)
-                BackHandler(onBack = {
-                    currentSelectedPuppy = null
-                })
+                BackHandler(
+                    onBack = {
+                        currentSelectedPuppy = null
+                    }
+                )
             }
         }
     }
@@ -86,7 +104,7 @@ fun MyApp(puppyViewModel: PuppyViewModel = viewModel()) {
 @Composable
 fun DemoPuppy() {
     val demo = Puppy().apply {
-        name = "Bailey";
+        name = "Bailey"
         imgId = R.mipmap.a
         age = "8 months"
         description =
@@ -171,19 +189,16 @@ fun PuppyItem(puppy: Puppy, onClick: () -> Unit) {
     }
 }
 
+@Composable
+fun LightPreview() {
+    MyTheme {
+        MyApp()
+    }
+}
 
-//@Composable
-//fun LightPreview() {
-//    MyTheme {
-//        MyApp()
-//    }
-//}
-//
-//@Composable
-//fun DarkPreview() {
-//    MyTheme(darkTheme = true) {
-//        MyApp()
-//    }
-//}
-
-
+@Composable
+fun DarkPreview() {
+    MyTheme(darkTheme = true) {
+        MyApp()
+    }
+}
--- app/src/main/java/com/example/androiddevchallenge/PuppyViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/PuppyViewModel.kt
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
-import androidx.compose.runtime.MutableState
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -13,32 +27,40 @@ class PuppyViewModel : ViewModel() {
     init {
         val list = mutableListOf<Puppy>()
         repeat(10) {
-            list.add(Puppy().apply {
-                name = "Bailey";
-                imgId = R.mipmap.a
-                age = "8 months"
-                description =
-                    "Meet Bailey! This playful dog is ready for her forever home. she is only 2 years old and has not quite figured out that she is not a puppy anymore. Bailey is going to do well in a home where she can get a ton of consistency and help becoming the best girl she be. If Bailey sounds like your speed we hope you will adopt her today!"
-            })
-            list.add(Puppy().apply {
-                name = "Jax";
-                imgId = R.mipmap.b
-                age = "7 months"
-                description =
-                    "Meet Jax! This playful pup is looking for his forever home. He is still very much a puppy and will need help in his new home to stay on track to becoming the best boy he can be. With some consistency and training he will do well in a wide variety of homes. Jax is going to do best in a home without small kids. If Jax sounds like he would fit into your life we hope you will adopt him today!"
-            })
-            list.add(Puppy().apply {
-                name = "Theodosia";
-                imgId = R.mipmap.c
-                age = "10 months"
-                description = "Say hello tot he one and only Theodosia! This spunky lady is looking for an active family willing to give her mental enrichment and physical activity! Theodosia does need to go into a home with no young kids, and no other dogs! She is a very friendly dog, but she can be a lot of dog if you are not dog savvy. She is about 5 years old, but she would still love to learn new things! She may benefit from basic training to learn new commands and manners. If you think Theodosia is the dog of your dreams, come on by and make it a reality!"
-            })
-            list.add(Puppy().apply {
-                name = "Stranger";
-                imgId = R.mipmap.d
-                age = "10 months"
-                description = "Looking for a young, sweet and active dog? Stranger might be the girl for you! She needs to go to a home without children under 5 years old, just to make sure no one gets knocked down when she happily romps through the house! Stranger is a very affectionate dog, but needs some training to help boost her confidence and to learn proper manners. Once Stranger is fully comfortable with her new family and home, she will make a wonderful companion. If you're looking forward to helping Stranger become more of a superstar every day, come in and meet her!"
-            })
+            list.add(
+                Puppy().apply {
+                    name = "Bailey"
+                    imgId = R.mipmap.a
+                    age = "8 months"
+                    description =
+                        "Meet Bailey! This playful dog is ready for her forever home. she is only 2 years old and has not quite figured out that she is not a puppy anymore. Bailey is going to do well in a home where she can get a ton of consistency and help becoming the best girl she be. If Bailey sounds like your speed we hope you will adopt her today!"
+                }
+            )
+            list.add(
+                Puppy().apply {
+                    name = "Jax"
+                    imgId = R.mipmap.b
+                    age = "7 months"
+                    description =
+                        "Meet Jax! This playful pup is looking for his forever home. He is still very much a puppy and will need help in his new home to stay on track to becoming the best boy he can be. With some consistency and training he will do well in a wide variety of homes. Jax is going to do best in a home without small kids. If Jax sounds like he would fit into your life we hope you will adopt him today!"
+                }
+            )
+            list.add(
+                Puppy().apply {
+                    name = "Theodosia"
+                    imgId = R.mipmap.c
+                    age = "10 months"
+                    description = "Say hello tot he one and only Theodosia! This spunky lady is looking for an active family willing to give her mental enrichment and physical activity! Theodosia does need to go into a home with no young kids, and no other dogs! She is a very friendly dog, but she can be a lot of dog if you are not dog savvy. She is about 5 years old, but she would still love to learn new things! She may benefit from basic training to learn new commands and manners. If you think Theodosia is the dog of your dreams, come on by and make it a reality!"
+                }
+            )
+            list.add(
+                Puppy().apply {
+                    name = "Stranger"
+                    imgId = R.mipmap.d
+                    age = "10 months"
+                    description = "Looking for a young, sweet and active dog? Stranger might be the girl for you! She needs to go to a home without children under 5 years old, just to make sure no one gets knocked down when she happily romps through the house! Stranger is a very affectionate dog, but needs some training to help boost her confidence and to learn proper manners. Once Stranger is fully comfortable with her new family and home, she will make a wonderful companion. If you're looking forward to helping Stranger become more of a superstar every day, come in and meet her!"
+                }
+            )
         }
         _puppys.value = list
     }
